dfc1eac2354af767b5238aae1f1121ae2bad0d99
Duncan <dun_edwards@yahoo.com>
2017-08-31 16:54:29 +0300

    Next cut, now we have a demo page

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
+
+render(
+  <MuiThemeProvider>
+    <div>
+      <h1>Start</h1>
+      <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHeaderColumn>ID</TableHeaderColumn>
+          <TableHeaderColumn>Name</TableHeaderColumn>
+          <TableHeaderColumn>Status</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableRowColumn>1</TableRowColumn>
+          <TableRowColumn>John Smith</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>2</TableRowColumn>
+          <TableRowColumn>Randal White</TableRowColumn>
+          <TableRowColumn>Unemployed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>3</TableRowColumn>
+          <TableRowColumn>Stephanie Sanders</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>4</TableRowColumn>
+          <TableRowColumn>Steve Brown</TableRowColumn>
+          <TableRowColumn>Employed</TableRowColumn>
+        </TableRow>
+        <TableRow>
+          <TableRowColumn>5</TableRowColumn>
+          <TableRowColumn>Christopher Nolan</TableRowColumn>
+          <TableRowColumn>Unemployed</TableRowColumn>
+        </TableRow>
+      </TableBody>
+    </Table>
+    </div>
+  </MuiThemeProvider>,
+  document.getElementById('app')
+);
